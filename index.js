81831b4077f8735313eabc05fa000fce6e4b60b4
Soikat07 <sahasoikat94@gmail.com>
2023-06-09 08:37:27 +0600

    select class collection add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -37,16 +37,42 @@ async function run() {
     });
 
     const classesCollection = client.db("summerCamp").collection("classes");
+    const selectClassesCollection = client.db("summerCamp").collection("selectClasses");
 
     // get all data from classes from database collection
     app.get('/classes', async (req, res) => {
       const result = await classesCollection.find().toArray();
       res.send(result);
     })
+    
     // get the popular classes from database collection
     app.get('/popularclasses', async (req, res) => {
       const query = { enrolled_students: { $gte: 10 } };
       const result = await classesCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    // select classes collection
+    app.get('/selectClass', async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await selectClassesCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post('/selectClass', async (req, res) => {
+      const item = req.body;
+      const result = await selectClassesCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete('/selectClass/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectClassesCollection.deleteOne(query);
       res.send(result);
     })
 
